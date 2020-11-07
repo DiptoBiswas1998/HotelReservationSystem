78e8f37590454b3149ea850446a8569d2fba0d16
unknown <diptobiswas1998@gmail.com>
2020-11-07 07:49:31 +0530

    [Dipto] Add: Adding Hotel name and rate in a Hotel Reservation System

--- HotelReservationSystem/HotelReservationSystem/Program.cs
+++ HotelReservationSystem/HotelReservationSystem/Program.cs
@@ -6,6 +6,10 @@ namespace HotelReservationSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Hotel Reservation System!");
+            HotelSystem hotelSystem = new HotelSystem();
+            hotelSystem.AddHotel(new Hotel("Hyath", 100));
+            hotelSystem.AddHotel(new Hotel("Taj", 150));
+            hotelSystem.AddHotel(new Hotel("Oberoi", 200));
         }
     }
 }
